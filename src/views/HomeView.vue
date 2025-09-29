4f5c3f1220d0602a071ea6b2e00a5c7035cc8d9f
Tiago Dariel <tdariel2@gmail.com>
2025-09-29 19:16:01 -0300

    route

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/list-icons">icons</RouterLink>
+      </nav>
+    </div>
+  </header>
 </template>
